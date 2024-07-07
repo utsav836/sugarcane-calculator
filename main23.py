88419bc985e0ccf590f8e4557313ccede5a94b23
Utsav khemariya <130468084+utsav836@users.noreply.github.com>
2024-07-07 08:56:58 +0530

    Update main23.py

--- main23.py
+++ main23.py
@@ -6,10 +6,11 @@ def sugarcane_calculator(net_weight, rate1, rate2, cane_rate):
     cane_amount = net_weight * cane_rate
     amount1 = net_weight * rate1
     amount2 = net_weight * rate2
-    amount = amount2 - amount1
+    final_amount = (cane_amount - (net_weight * rate1) - (net_weight * rate2))
+    return amount1, amount2,  cane_amount, final_amount
    
-    final_amount = cane_amount - amount
-    return amount1, amount2, amount, cane_amount, final_amount
+   
+   
 
 # Function to create SQLite connection and store results
 def store_result(net_weight, rate1, rate2, cane_rate, amount1, amount2, amount, cane_amount, final_amount):
